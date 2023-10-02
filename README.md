1421fccfad92a083a5c73b42ad4389a5378d854a
Andres F. Silvera <andresfsilverall0109@gmail.com>
2023-10-02 13:04:11 -0500

    se cambio elnombre de la BD

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-# Top 20 animes API GraphQ
+# Top 20 animes API GraphQL
 
-![GraphQL]("assets/GraphQL.png"){width=200}
+![GraphQL](assets/GraphQL.png){width=200}
 
 Api basada en la arquitectura de GraphQL, en la que el usuario puede consultar el top 20 de los mejores animes del genero shōnen, además permite listar las consulas, agregar, actualizar y eliminar recursos.
--- config/settings.py
+++ config/settings.py
@@ -1,3 +1,6 @@
+from pathlib import Path
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -68,7 +71,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': 'animes.db',
     }
 }
 
@@ -90,6 +93,12 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
+
+
+GRAPHENE = {
+    "SCHEMA": "django_root.schema.schema"
+}
 
 
 # Internationalization
